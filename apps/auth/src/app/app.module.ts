a2e414546920a491e3830e11138c169b9915e0cf
ericsison <ericsison.dev@gmail.com>
2022-12-22 22:31:42 +0800

    project initial setup

--- apps/auth/src/app/app.module.ts
+++ apps/auth/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // config module for reading enironment variables
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
 })
 export class AppModule {}
