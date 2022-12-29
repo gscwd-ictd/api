0cae3fa726d65e05519bf0dfc35ed9b063100407
ericsison <ericsison.dev@gmail.com>
2022-12-30 00:22:26 +0800

    segregated common functions to their respective services

--- apps/ims/src/api/item/components/specification/core/specification.controller.ts
+++ apps/ims/src/api/item/components/specification/core/specification.controller.ts
@@ -1,5 +1,5 @@
 import { ICrudRoutes } from '@gscwd-api/crud';
-import { BadRequestException, Controller, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Controller, Delete, Get, NotFoundException, Post, Put } from '@nestjs/common';
 import { UpdateResult, DeleteResult } from 'typeorm';
 import { CreateItemSpecificationDto } from '../data/specification.dto';
 import { ItemSpecification } from '../data/specification.entity';
@@ -9,22 +9,27 @@ import { SpecificationService } from './specification.service';
 export class SpecificationController implements ICrudRoutes {
   constructor(private readonly specificationService: SpecificationService) {}
 
+  @Post()
   async create(data: CreateItemSpecificationDto): Promise<ItemSpecification> {
     return await this.specificationService.create(data, () => new BadRequestException());
   }
 
+  @Get()
   async findAll(): Promise<ItemSpecification[]> {
     return await this.specificationService.findAll();
   }
 
+  @Get(':id')
   async findById(id: string): Promise<ItemSpecification> {
     return await this.specificationService.findOneBy({ id }, () => new NotFoundException());
   }
 
+  @Put(':id')
   async update(id: string, data: unknown): Promise<UpdateResult> {
     return await this.specificationService.update({ id }, data, () => new BadRequestException());
   }
 
+  @Delete(':id')
   async delete(id: string): Promise<DeleteResult> {
     return await this.specificationService.delete({ id }, () => new BadRequestException());
   }
--- apps/ims/src/api/item/core/item.controller.ts
+++ apps/ims/src/api/item/core/item.controller.ts
@@ -1,17 +1,59 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
-import { ItemService } from './item.service';
+import { BadRequestException, Controller, Get, Param, Query } from '@nestjs/common';
+import { CategoryService } from '../components/category';
+import { CharacteristicService } from '../components/characteristic';
+import { ClassificationService } from '../components/classification';
+import { SpecificationService } from '../components/specification';
 
 @Controller('item-info')
 export class ItemController {
-  constructor(private readonly itemService: ItemService) {}
+  constructor(
+    // inject characteristic service
+    private readonly characteristicService: CharacteristicService,
+
+    // inject classification service
+    private readonly classificationService: ClassificationService,
+
+    // inject category service
+    private readonly categoryService: CategoryService,
+
+    // inject specification service
+    private readonly specificationService: SpecificationService
+  ) {}
 
   @Get('classification')
-  async findAllClassificationsByCharacteristic(@Query('characteristic') name: string) {
-    return await this.itemService.findAllClassifications(name);
+  async findAllClassificationsByCharacteristic(@Query('characteristic') characteristicName: string) {
+    return await this.classificationService.findAllClassification(characteristicName);
   }
 
   @Get('classification/:id')
   async findClassificationByCharacteristic(@Param('id') classificationId: string) {
-    return await this.itemService.findClassification(classificationId);
+    return await this.classificationService.findClassification(classificationId);
+  }
+
+  @Get('categories')
+  async findAllCategoriesByClassification(@Query('classification') classificationName?: string, @Query('unit') unitName?: string) {
+    // check if classification is specified as query
+    if (classificationName) return await this.categoryService.findAllCategoriesByClassification(classificationName);
+
+    // check if unit is specified as query
+    if (unitName) return await this.categoryService.findAllCategoriesByMeasurementUnit(unitName);
+
+    // throw exception if there is not query at all
+    throw new BadRequestException();
+  }
+
+  @Get('categories/:id')
+  async findCategoryByClassification(@Param('id') categoryId: string) {
+    return await this.categoryService.findCategory(categoryId);
+  }
+
+  @Get('specifications')
+  async findAllSpecificationsByCategory(@Query('category') categoryName: string) {
+    return await this.specificationService.findAllSpecifications(categoryName);
+  }
+
+  @Get('specifications/:id')
+  async findSpecificationByCategory(@Param('id') specificationId: string) {
+    return await this.specificationService.findSpecification(specificationId);
   }
 }
