06955c6d6e97c709c7d16f58de76c60c36892fb4
ericsison <ericsison.dev@gmail.com>
2022-12-29 22:35:47 +0800

    fixed unit id and added dependencies to item service

--- apps/ims/src/api/item/core/item.service.ts
+++ apps/ims/src/api/item/core/item.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { CategoryService } from '../components/category';
+import { CharacteristicService } from '../components/characteristic';
+import { ClassificationService } from '../components/classification';
+import { SpecificationService } from '../components/specification';
 
 @Injectable()
-export class ItemService {}
+export class ItemService {
+  constructor(
+    // inject characteristic service
+    private readonly characteristicService: CharacteristicService,
+
+    // inject classification service
+    private readonly classificationService: ClassificationService,
+
+    // inject category service
+    private readonly categoryService: CategoryService,
+
+    // inject specification service
+    private readonly specificationService: SpecificationService
+  ) {}
+}
