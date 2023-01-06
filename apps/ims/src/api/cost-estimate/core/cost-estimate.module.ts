d02ad94116fd46528f9806f6f52b9de106cdaed9
taypskript <alfeche.ictd@gmail.com>
2023-01-06 14:17:42 +0800

    add labor cost and labor type module

--- apps/ims/src/api/cost-estimate/core/cost-estimate.module.ts
+++ apps/ims/src/api/cost-estimate/core/cost-estimate.module.ts
@@ -3,9 +3,11 @@ import { EquipmentCostModule } from '../components/equipment-cost';
 import { ProjectDetailModule } from '../components/project-details/core/project-detail.module';
 import { CostEstimateController } from './cost-estimate.controller';
 import { CostEstimateService } from './cost-estimate.service';
+import { LaborTypeModule } from '../components/labor-type';
+import { LaborCostModule } from '../components/labor-cost';
 
 @Module({
-  imports: [ProjectDetailModule, EquipmentCostModule],
+  imports: [ProjectDetailModule, EquipmentCostModule, LaborCostModule, LaborTypeModule],
   providers: [CostEstimateService],
   controllers: [CostEstimateController],
 })
