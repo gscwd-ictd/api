e41c3a021b9c35f0d7dc823d1eb196113860ec4b
ericsison <ericsison.dev@gmail.com>
2023-01-09 10:32:54 +0800

    fixed config module env path

--- apps/ims/src/api/app.module.ts
+++ apps/ims/src/api/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { DatabaseConfig } from '../config';
 import { API_MODULES } from '../constants';
 
 @Module({
   imports: [
     // config module for reading enironment variables
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: join(__dirname, '../../../apps/ims/.env') }),
 
     // database connection via typeorm
     TypeOrmModule.forRootAsync({ useClass: DatabaseConfig }),
