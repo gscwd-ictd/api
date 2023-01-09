920843c4f7130edbd2973f9fa0f11fdb40f9741e
taypskript <alfeche.ictd@gmail.com>
2023-01-09 16:32:03 +0800

    added promise type on labor type controller

--- apps/ims/src/api/cost-estimate/components/labor-type/core/labor-type.controller.ts
+++ apps/ims/src/api/cost-estimate/components/labor-type/core/labor-type.controller.ts
@@ -1,34 +1,53 @@
-import { Controller, Get, Post, Body, Param, Delete, Put, BadRequestException, NotFoundException } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+  BadRequestException,
+  NotFoundException,
+  Query,
+  DefaultValuePipe,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { LaborTypeService } from './labor-type.service';
 import { CreateLaborTypeDto, UpdateLaborTypeDto } from '../data/labor-type.dto';
 import { ICrudRoutes } from '@gscwd-api/crud';
+import { LaborType } from '../data/labor-type.entity';
+import { Pagination } from 'nestjs-typeorm-paginate';
+import { DeleteResult, UpdateResult } from 'typeorm';
 
 @Controller({ version: '1', path: 'labor-types' })
 export class LaborTypeController implements ICrudRoutes {
   constructor(private readonly laborTypeService: LaborTypeService) {}
 
   @Post()
-  async create(@Body() data: CreateLaborTypeDto) {
+  async create(@Body() data: CreateLaborTypeDto): Promise<LaborType> {
     return await this.laborTypeService.create(data, () => new BadRequestException());
   }
 
   @Get()
-  async findAll() {
-    return await this.laborTypeService.findAll();
+  async findAll(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number
+  ): Promise<Pagination<LaborType>> {
+    return await this.laborTypeService.findAll({ pagination: { page, limit } });
   }
 
   @Get(':id')
-  async findById(@Param('id') id: string) {
+  async findById(@Param('id') id: string): Promise<LaborType> {
     return this.laborTypeService.findOneBy({ id }, () => new NotFoundException());
   }
 
   @Put(':id')
-  async update(@Param('id') id: string, @Body() data: UpdateLaborTypeDto) {
+  async update(@Param('id') id: string, @Body() data: UpdateLaborTypeDto): Promise<UpdateResult> {
     return this.laborTypeService.update({ id }, data, () => new BadRequestException());
   }
 
   @Delete(':id')
-  async delete(@Param('id') id: string) {
+  async delete(@Param('id') id: string): Promise<DeleteResult> {
     return this.laborTypeService.delete({ id }, () => new BadRequestException());
   }
 }
