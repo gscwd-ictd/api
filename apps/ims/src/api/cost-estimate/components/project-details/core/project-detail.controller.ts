c5878ccd72de41e54b64f2ad1003a621fb71a7c6
taypskript <alfeche.ictd@gmail.com>
2023-01-09 16:38:46 +0800

    added promise type on project-detail controller

--- apps/ims/src/api/cost-estimate/components/project-details/core/project-detail.controller.ts
+++ apps/ims/src/api/cost-estimate/components/project-details/core/project-detail.controller.ts
@@ -1,34 +1,53 @@
-import { BadRequestException, Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { ICrudRoutes } from '@gscwd-api/crud';
 import { ProjectDetailService } from './project-detail.service';
 import { CreateProjectDetailDto, UpdateProjectDetailDto } from '../data/project-detail.dto';
+import { ProjectDetail } from '../data/project-detail.entity';
+import { Pagination } from 'nestjs-typeorm-paginate';
+import { DeleteResult, UpdateResult } from 'typeorm';
 
 @Controller({ version: '1', path: 'cost-estimates/project-details' })
 export class ProjectDetailController implements ICrudRoutes {
   constructor(private readonly projectDetailService: ProjectDetailService) {}
 
   @Post()
-  async create(@Body() data: CreateProjectDetailDto) {
+  async create(@Body() data: CreateProjectDetailDto): Promise<ProjectDetail> {
     return await this.projectDetailService.create(data, () => new BadRequestException());
   }
 
   @Get()
-  async findAll() {
-    return await this.projectDetailService.findAll();
+  async findAll(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number
+  ): Promise<Pagination<ProjectDetail>> {
+    return await this.projectDetailService.findAll({ pagination: { page, limit } });
   }
 
   @Get(':id')
-  async findById(@Param('id') id: string) {
+  async findById(@Param('id') id: string): Promise<ProjectDetail> {
     return await this.projectDetailService.findOneBy({ id }, () => new NotFoundException());
   }
 
   @Put(':id')
-  async update(@Param('id') id: string, @Body() data: UpdateProjectDetailDto) {
+  async update(@Param('id') id: string, @Body() data: UpdateProjectDetailDto): Promise<UpdateResult> {
     return await this.projectDetailService.update({ id }, data, () => new BadRequestException());
   }
 
   @Delete(':id')
-  async delete(@Param('id') id: string) {
+  async delete(@Param('id') id: string): Promise<DeleteResult> {
     return await this.projectDetailService.delete({ id }, () => new BadRequestException());
   }
 }
