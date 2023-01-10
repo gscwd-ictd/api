813542e39bae5a59059e22ae860b8332f985a09e
ericsison <ericsison.dev@gmail.com>
2023-01-10 20:50:11 +0800

    removed getRepository() function in crud helper, since it's already in crud service

--- libs/crud/src/lib/crud.helper.ts
+++ libs/crud/src/lib/crud.helper.ts
@@ -4,10 +4,6 @@ import { ObjectLiteral } from 'typeorm';
 export abstract class CrudHelper<T extends ObjectLiteral> {
   constructor(private readonly crud: CrudService<T>) {}
 
-  public getRepository() {
-    return this.crud.getRepository();
-  }
-
   public getProvider() {
     return this.crud;
   }
