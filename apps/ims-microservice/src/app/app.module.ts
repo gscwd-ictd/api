f34780550a613fbb9c68971cc713fe7af517aa55
ericsison <ericsison.dev@gmail.com>
2023-01-11 16:03:49 +0800

    added database configuration

--- apps/ims-microservice/src/app/app.module.ts
+++ apps/ims-microservice/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // config module for reading enironment variables
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: join(__dirname, '../../../apps/ims-microservice/.env') }),
+  ],
 })
 export class AppModule {}
