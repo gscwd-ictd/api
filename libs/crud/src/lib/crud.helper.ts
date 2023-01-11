7cdc2a8157e38144436de93a78fa891f94b2574b
ericsison <ericsison.dev@gmail.com>
2023-01-11 08:51:34 +0800

    code refactoring

--- libs/crud/src/lib/crud.helper.ts
+++ libs/crud/src/lib/crud.helper.ts
@@ -1,4 +1,4 @@
-import { CrudService } from '../../../crud/src/lib/crud.service';
+import { CrudService } from '../lib/crud.service';
 import { ObjectLiteral } from 'typeorm';
 
 export abstract class CrudHelper<T extends ObjectLiteral> {
