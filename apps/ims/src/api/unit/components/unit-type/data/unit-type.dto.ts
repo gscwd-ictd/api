aedbd69cae9303b419dcb9e27f2344a25b40071d
ericsison <ericsison.dev@gmail.com>
2023-01-16 13:27:39 +0800

    updated create and update dto and added decorators for validation. also added partial type and omit type for update dto

--- apps/ims/src/api/unit/components/unit-type/data/unit-type.dto.ts
+++ apps/ims/src/api/unit/components/unit-type/data/unit-type.dto.ts
@@ -1,6 +1,13 @@
+import { PartialType } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
+
 export class CreateUnitTypeDto {
+  @IsString()
   type: string;
+
+  @IsOptional()
+  @IsString()
   description: string;
 }
 
-export class UpdateUnitTypeDto extends CreateUnitTypeDto {}
+export class UpdateUnitTypeDto extends PartialType(CreateUnitTypeDto) {}
