1ccaa43378a69401a758bd1aea00c39c80cb1bee
ericsison <ericsison.dev@gmail.com>
2023-01-16 13:36:48 +0800

    reverted pulled code in finance module

--- apps/finance/src/api/budget/components/material-cost/core/material-cost.service.ts
+++ apps/finance/src/api/budget/components/material-cost/core/material-cost.service.ts
@@ -1,13 +1,4 @@
-import { IMS_MICROSERVICE, MicroserviceHelper } from '@gscwd-api/microservices';
-import { Inject, Injectable } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MaterialCostService extends MicroserviceHelper {
-  constructor(
-    @Inject(IMS_MICROSERVICE)
-    private readonly client: ClientProxy
-  ) {
-    super(client);
-  }
-}
+export class MaterialCostService {}
