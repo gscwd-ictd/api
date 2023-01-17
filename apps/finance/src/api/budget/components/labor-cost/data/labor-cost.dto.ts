81e6caa7d235368bb67bf3c6a3c3d4a6d18f3c60
taypskript <alfeche.ictd@gmail.com>
2023-01-17 10:42:00 +0800

    added class validator

--- apps/finance/src/api/budget/components/labor-cost/data/labor-cost.dto.ts
+++ apps/finance/src/api/budget/components/labor-cost/data/labor-cost.dto.ts
@@ -1,16 +1,22 @@
+import { OmitType, PartialType } from '@nestjs/swagger';
+import { IsCurrency, IsDecimal, IsInt, IsUUID } from 'class-validator';
 import { ProjectDetail } from '../../project-detail';
 
 export class CreateLaborCostDto {
+  @IsUUID()
   projectDetail: ProjectDetail;
+
+  @IsInt()
   numberOfPerson: number;
+
+  @IsInt()
   numberOfDay: number;
+
+  @IsDecimal()
   unitCost: number;
+
+  @IsCurrency()
   amount: number;
 }
 
-export class UpdateLaborCostDto {
-  numberOfPerson: number;
-  numberOfDay: number;
-  unitCost: number;
-  amount: number;
-}
+export class UpdateLaborCostDto extends PartialType(OmitType(CreateLaborCostDto, ['projectDetail'] as const)) {}
