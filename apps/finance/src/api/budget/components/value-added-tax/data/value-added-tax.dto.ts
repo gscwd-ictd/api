41c41321d7b193ebe6d196ce91ced9597c923db8
taypskript <alfeche.ictd@gmail.com>
2023-01-17 10:47:10 +0800

    added class validator

--- apps/finance/src/api/budget/components/value-added-tax/data/value-added-tax.dto.ts
+++ apps/finance/src/api/budget/components/value-added-tax/data/value-added-tax.dto.ts
@@ -1,10 +1,13 @@
+import { OmitType, PartialType } from '@nestjs/swagger';
+import { IsDecimal, IsUUID } from 'class-validator';
 import { ProjectDetail } from '../../project-detail';
 
 export class CreateValueAddedTaxDto {
+  @IsUUID()
   projectDetail: ProjectDetail;
+
+  @IsDecimal()
   percentage: number;
 }
 
-export class UpdateValueAddedTaxDto {
-  percentage: number;
-}
+export class UpdateValueAddedTaxDto extends PartialType(OmitType(CreateValueAddedTaxDto, ['projectDetail'] as const)) {}
