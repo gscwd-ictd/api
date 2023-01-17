4e3c79ffa8be0c0dfab8cf5447c1129934e52451
taypskript <alfeche.ictd@gmail.com>
2023-01-17 10:46:04 +0800

    added class validator

--- apps/finance/src/api/budget/components/labor-type/data/labor-type.dto.ts
+++ apps/finance/src/api/budget/components/labor-type/data/labor-type.dto.ts
@@ -1,5 +1,10 @@
+import { PartialType } from '@nestjs/swagger';
+import { IsString, MaxLength } from 'class-validator';
+
 export class CreateLaborTypeDto {
+  @IsString()
+  @MaxLength(50, { message: 'Description name is too long.' })
   description: string;
 }
 
-export class UpdateLaborTypeDto extends CreateLaborTypeDto {}
+export class UpdateLaborTypeDto extends PartialType(CreateLaborTypeDto) {}
