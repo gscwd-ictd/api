ebd844132bbe4ba7dad20d539bbddf44cf4a36a1
ericsison <ericsison.dev@gmail.com>
2023-01-18 11:49:55 +0800

    added authentication type for auth patterns

--- libs/microservices/src/types/ms.types.ts
+++ libs/microservices/src/types/ms.types.ts
@@ -2,6 +2,6 @@ import { AuthPatterns, ItemPatterns } from '../constants';
 import { ObjectValues } from '../utils';
 
 // microservice target types
-export type Items = ObjectValues<typeof ItemPatterns>;
-
 export type Authentication = ObjectValues<typeof AuthPatterns>;
+
+export type Items = ObjectValues<typeof ItemPatterns>;
