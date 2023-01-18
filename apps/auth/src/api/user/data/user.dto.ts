60316f1912600b0bbcb06aeb298c8b3e557b66c2
ericsison <ericsison.dev@gmail.com>
2023-01-18 10:10:16 +0800

    refactored code

--- apps/auth/src/api/user/data/user.dto.ts
+++ apps/auth/src/api/user/data/user.dto.ts
@@ -1,4 +1,12 @@
+import { PartialType } from '@nestjs/swagger';
+import { IsEmail, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail()
   email: string;
+
+  @IsString()
   password: string;
 }
+
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
