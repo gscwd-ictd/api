afa1818136ae0f1a67358f233f24e0d7585d886f
ericsison <ericsison.dev@gmail.com>
2023-01-18 16:21:03 +0800

    refactored code

--- libs/microservices/src/utils/ms.utils.ts
+++ libs/microservices/src/utils/ms.utils.ts
@@ -5,11 +5,22 @@ import { RpcError } from './error';
 export type MessagePattern<T> = { msg: T };
 
 // used for composing object with strongly typed string values
-export type ObjectValues<T> = T[keyof T];
+//export type ObjectValues<T> = T[keyof T];
 
-// the microservice request object
+/**
+ * Microservice request object
+ *
+ * @param pattern A microservice channel to route the request to its host function.
+ * This pattern should also reside in the listener (in this case, the host),
+ * to call the appropriate function.
+ *
+ * @param payload Input data that the called function needs to process the requested function.
+ *
+ * @param onError A callback function in case an error in the request happens.
+ *
+ */
 export type RpcRequest<T, K> = {
-  target: K;
-  payload: T;
+  pattern: T;
+  payload: K;
   onError?: (error: RpcError) => HttpException;
 };
