125cdbcf693a58358ca1750046beb420ac8d8d54
ericsison <ericsison.dev@gmail.com>
2023-01-23 23:20:28 +0800

    modified barrel file to add more entry points to this library

--- libs/microservices/src/index.ts
+++ libs/microservices/src/index.ts
@@ -1,3 +1,10 @@
-export * from './constants';
-export * from './lib';
-export * from './utils';
+export * from './utils/ms-hybrid';
+export * from './utils/ms-provider';
+export * from './utils/my-rpc';
+
+export * from './lib/ms-client.service';
+
+export * from './types/ms.types';
+
+export * from './patterns/items';
+export * from './patterns/units';
