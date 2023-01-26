da35f4dbe525315c4a8985df810b013da7a8d4e0
ericsison <ericsison.dev@gmail.com>
2023-01-26 08:41:13 +0800

    removed quantity in item and ppe specifications

--- libs/app-entities/src/lib/databases/items/data/ppe/components/specifications/specifications.entity.ts
+++ libs/app-entities/src/lib/databases/items/data/ppe/components/specifications/specifications.entity.ts
@@ -23,9 +23,6 @@ export class PpeSpecification extends DatabaseEntity implements IEntity {
   @Column({ length: 100 })
   details: string;
 
-  @Column()
-  quantity: number;
-
   @Column({ name: 'reorder_point' })
   reorderPoint: number;
 
