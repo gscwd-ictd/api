18d71a1383a940f3f5e14c163275af1b6a36f073
ericsison <ericsison.dev@gmail.com>
2023-01-27 14:11:54 +0800

    transferred microservice client calls from controller to service

--- apps/ims/src/app/services/items/core/items.controller.ts
+++ apps/ims/src/app/services/items/core/items.controller.ts
@@ -1,31 +1,21 @@
-import { ItemDetailsViewPatterns, MicroserviceClient } from '@gscwd-api/microservices';
-import { Controller, Get, HttpException, Param, ParseUUIDPipe, Query, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe, Query, UseInterceptors } from '@nestjs/common';
 import { FindAllItemsInterceptor, FindItemByIdInterceptor } from '../../../../global/interceptors';
 import { IPaginationOptions } from 'nestjs-typeorm-paginate';
+import { ItemsService } from './items.service';
 
 @Controller({ version: '1', path: 'items' })
 export class ItemsController {
-  constructor(private readonly microserviceClient: MicroserviceClient) {}
+  constructor(private readonly itemsService: ItemsService) {}
 
   @UseInterceptors(FindAllItemsInterceptor)
   @Get()
-  async findAllItemsFromView(@Query() { page, limit }: IPaginationOptions) {
-    return await this.microserviceClient.call({
-      action: 'send',
-      pattern: ItemDetailsViewPatterns.FIND_ALL,
-      payload: { page, limit },
-      onError: ({ code, message, details }) => new HttpException(message, code, { cause: details as Error }),
-    });
+  async findAll(@Query() options: IPaginationOptions) {
+    return await this.itemsService.findAllItemsFromView(options);
   }
 
   @UseInterceptors(FindItemByIdInterceptor)
   @Get(':id')
-  async findItemFromViewById(@Param('id', ParseUUIDPipe) id: string) {
-    return await this.microserviceClient.call({
-      action: 'send',
-      pattern: ItemDetailsViewPatterns.FIND_BY_ID,
-      payload: { id },
-      onError: ({ code, message, details }) => new HttpException(message, code, { cause: details as Error }),
-    });
+  async findById(@Param('id', ParseUUIDPipe) id: string) {
+    return await this.itemsService.findItemFromViewById(id);
   }
 }
--- apps/ims/src/app/services/items/core/items.service.ts
+++ apps/ims/src/app/services/items/core/items.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { ItemsView, MicroserviceClient } from '@gscwd-api/microservices';
+import { HttpException, Injectable } from '@nestjs/common';
+import { IPaginationOptions } from 'nestjs-typeorm-paginate';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  constructor(private readonly microserviceClient: MicroserviceClient) {}
+
+  async findAllItemsFromView({ page, limit }: IPaginationOptions) {
+    return await this.microserviceClient.call({
+      action: 'send',
+      pattern: ItemsView.FIND_ALL,
+      payload: { page, limit },
+      onError: ({ code, message, details }) => new HttpException(message, code, { cause: details as Error }),
+    });
+  }
+
+  async findItemFromViewById(id: string) {
+    return await this.microserviceClient.call({
+      action: 'send',
+      pattern: ItemsView.FIND_BY_ID,
+      payload: { id },
+      onError: ({ code, message, details }) => new HttpException(message, code, { cause: details as Error }),
+    });
+  }
+}
