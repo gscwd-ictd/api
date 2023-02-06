b3500ca2ed6eb1fdec6513ce79750ae8d8accba6
ericsison <ericsison.dev@gmail.com>
2023-02-06 13:15:10 +0800

    initial project setup

--- apps/procurement/src/app/app.module.ts
+++ apps/procurement/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { DatabaseConfig } from '../config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // config module setup for reading env variables
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: join(__dirname, '../../../apps/procurement/.env') }),
+
+    // typeorm config to connect to postgres db
+    TypeOrmModule.forRootAsync({ useClass: DatabaseConfig }),
+  ],
 })
 export class AppModule {}
