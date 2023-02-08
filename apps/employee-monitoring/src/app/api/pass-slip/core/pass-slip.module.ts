622dbde2599871e712d62818f70ca1f5541d272e
rvnarvaiza <rvnarvaiza@gmail.com>
2023-02-08 08:49:40 +0800

    Added Pass Slip Post and Get Routes

--- apps/employee-monitoring/src/app/api/pass-slip/core/pass-slip.module.ts
+++ apps/employee-monitoring/src/app/api/pass-slip/core/pass-slip.module.ts
@@ -1,13 +1,28 @@
 import { CrudModule } from '@gscwd-api/crud';
+import { MicroserviceClient, MS_CLIENT } from '@gscwd-api/microservices';
 import { PassSlip } from '@gscwd-api/models';
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { PassSlipApprovalModule } from '../components/approval/core/pass-slip-approval.module';
 import { PassSlipController } from './pass-slip.controller';
 import { PassSlipService } from './pass-slip.service';
 
 @Module({
-  imports: [CrudModule.register(PassSlip), PassSlipApprovalModule],
-  providers: [PassSlipService],
+  imports: [
+    CrudModule.register(PassSlip),
+    PassSlipApprovalModule,
+    ClientsModule.register([
+      {
+        name: MS_CLIENT,
+        transport: Transport.REDIS,
+        options: {
+          host: process.env.EMPLOYEE_REDIS_HOST,
+          port: parseInt(process.env.EMPLOYEE_REDIS_PORT),
+        },
+      },
+    ]),
+  ],
+  providers: [PassSlipService, MicroserviceClient],
   controllers: [PassSlipController],
   exports: [PassSlipService],
 })
