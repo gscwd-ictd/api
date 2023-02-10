ed8d73f41f0a16b4978ac2a0f0a49759e5104f57
ericsison <ericsison.dev@gmail.com>
2023-02-10 13:12:08 +0800

    added modules

---
+++ apps/procurement/src/constants/modules.ts
@@ -0,0 +1,5 @@
+import { PurchaseRequisitionModule } from '../app/api/purchase/components/requisition';
+import { OrgStructureModule } from '../app/services/hrms/components/org-structure';
+import { ItemsModule } from '../app/services/items';
+
+export const API_MODULES = [PurchaseRequisitionModule, OrgStructureModule, ItemsModule];
