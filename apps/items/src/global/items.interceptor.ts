a58f1bbbf8e3f1bda0a4d2798e46f2ec4e001788
ericsison <ericsison.dev@gmail.com>
2023-02-17 08:12:04 +0800

    added item summary interceptor

--- apps/items/src/global/items.interceptor.ts
+++ apps/items/src/global/items.interceptor.ts
@@ -32,8 +32,14 @@ export class FindItemSummaryInterceptor implements NestInterceptor {
         code: `${item.characteristic_code}-${item.classification_code}-${item.category_code}-${item.specification_code}`,
         classification: item.classification_name,
         item: item.category_name,
+        unit: item.unit_symbol,
         details: item.specification_name,
         description: item.description,
+        balance: item.balance,
+        reorder: {
+          point: item.reorder_point,
+          quantity: item.reorder_quantity,
+        },
       }))
     );
   }
@@ -58,10 +64,22 @@ export class FindItemInfoByIdInterceptor implements NestInterceptor {
             quantity: item.reorder_quantity,
           },
         },
-        meta: {
-          createdAt: item.created_at,
-          updatedAt: item.updated_at,
-        },
+        createdAt: item.created_at,
+        updatedAt: item.updated_at,
+      }))
+    );
+  }
+}
+
+@Injectable()
+export class GetItemBalanceInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler<unknown>): Observable<unknown> | Promise<Observable<unknown>> {
+    return next.handle().pipe(
+      map((item: ItemsView) => ({
+        code: `${item.characteristic_code}-${item.classification_code}-${item.category_code}-${item.specification_code}`,
+        item: item.category_name,
+        details: item.specification_name,
+        balance: item.balance,
       }))
     );
   }
