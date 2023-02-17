bd36eb694533aa649a94792360d7cadd8470f618
ericsison <ericsison.dev@gmail.com>
2023-02-17 08:08:55 +0800

    modified API_MODULES

--- apps/procurement/src/constants/modules.ts
+++ apps/procurement/src/constants/modules.ts
@@ -1,5 +1,14 @@
-import { PurchaseRequisitionModule } from '../app/api/purchase/components/requisition';
+import { RfqModule } from '../app/api/purchase/components/quotation';
+import { PrModule } from '../app/api/purchase/components/request/core/pr.module';
 import { OrgStructureModule } from '../app/services/hrms/components/org-structure';
 import { ItemsModule } from '../app/services/items';
 
-export const API_MODULES = [PurchaseRequisitionModule, OrgStructureModule, ItemsModule];
+export const API_MODULES = [
+  // api modules
+  PrModule,
+  RfqModule,
+
+  // microservice modules
+  OrgStructureModule,
+  ItemsModule,
+];
