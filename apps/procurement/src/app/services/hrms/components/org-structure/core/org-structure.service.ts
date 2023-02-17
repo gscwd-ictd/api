00a5bf4b414eae32e68b038670c573081ccce22e
ericsison <ericsison.dev@gmail.com>
2023-02-17 08:10:52 +0800

    modified hrms service

--- apps/procurement/src/app/services/hrms/components/org-structure/core/org-structure.service.ts
+++ apps/procurement/src/app/services/hrms/components/org-structure/core/org-structure.service.ts
@@ -1,6 +1,6 @@
 import { MicroserviceClient, OrganizationalStructurePatterns } from '@gscwd-api/microservices';
 import { OrganizationalStructure, OrganizationEntity } from '@gscwd-api/utils';
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class OrgStructureService {
@@ -14,11 +14,13 @@ export class OrgStructureService {
     })) as OrganizationalStructure;
   }
 
-  async getOrgUnitById(id: string) {
+  async getOrgUnitById(id: string): Promise<OrganizationEntity> {
     return (await this.microserviceClient.call({
       action: 'send',
       pattern: OrganizationalStructurePatterns.GET_ORG_BY_ID,
       payload: id,
+      onError: ({ message, code }) =>
+        new HttpException({ message: `Failed to find org with id: ${id}`, error: message }, code, { cause: new Error() }),
     })) as OrganizationEntity;
   }
 }
