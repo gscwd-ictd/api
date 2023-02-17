88f0f4dc4f5f7283c65aac1e04ec130781c53101
ericsison <ericsison.dev@gmail.com>
2023-02-17 08:12:56 +0800

    added item balance

--- apps/items/src/app/services/items/core/items.controller.ts
+++ apps/items/src/app/services/items/core/items.controller.ts
@@ -1,7 +1,12 @@
 import { ItemsViewPatterns } from '@gscwd-api/microservices';
 import { Controller, UseInterceptors } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
-import { FindAllItemSummariesInterceptor, FindItemInfoByIdInterceptor, FindItemSummaryInterceptor } from '../../../../global/';
+import {
+  FindAllItemSummariesInterceptor,
+  FindItemInfoByIdInterceptor,
+  FindItemSummaryInterceptor,
+  GetItemBalanceInterceptor,
+} from '../../../../global/';
 import { ItemsService } from './items.service';
 
 @Controller()
@@ -24,5 +29,11 @@ export class ItemsController {
   @MessagePattern(ItemsViewPatterns.FIND_SUMMARY_BY_ID)
   async findOneSummaryBy(@Payload('id') id: string) {
     return await this.itemsService.findOneBy(id);
+  }
+
+  @UseInterceptors(GetItemBalanceInterceptor)
+  @MessagePattern(ItemsViewPatterns.GET_ITEM_BALANCE)
+  async getBalanceById(@Payload('id') id: string) {
+    return await this.itemsService.getItemBalance(id);
   }
 }
