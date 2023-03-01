42bc0f9052643dcc6d2a528edbec3a6eebbfb692
ericsison <ericsison.dev@gmail.com>
2023-03-01 21:55:21 +0800

    added purchase-type in the barrel file

--- libs/models/src/lib/databases/procurement/index.ts
+++ libs/models/src/lib/databases/procurement/index.ts
@@ -2,6 +2,3 @@ export * from './data/purchase-request';
 export * from './data/requested-item';
 export * from './data/request-for-quotation';
 export * from './data/purchase-type';
-export * from './data/pr-code-seq';
-
-export * from './functions';
