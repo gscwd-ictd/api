3f86dbac25b9fd1e8967b586f875b694e88fd1d0
ericsison <ericsison.dev@gmail.com>
2023-03-08 09:39:08 +0800

    modified name of interceptor

--- apps/procurement/src/app/api/purchase/components/purchase-request/core/purchase-request.controller.ts
+++ apps/procurement/src/app/api/purchase/components/purchase-request/core/purchase-request.controller.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
 import { CreatePrDto } from '../data/pr.dto';
-import { PurchaseRequestInterceptor } from '../misc/pr.interceptor';
+import { CreatePurchaseRequestInterceptor } from '../misc/create-pr.interceptor';
 import { PurchaseRequestService } from './purchase-request.service';
 
 @Controller({ version: '1', path: 'pr' })
 export class PurchaseRequestController {
   constructor(private readonly prService: PurchaseRequestService) {}
 
-  @UseInterceptors(PurchaseRequestInterceptor)
+  @UseInterceptors(CreatePurchaseRequestInterceptor)
   @Post()
   async create(@Body() prDto: CreatePrDto) {
     return await this.prService.createRawPr(prDto);
