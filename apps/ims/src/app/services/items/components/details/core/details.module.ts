ef7f4daf539b047cf3b14d3983cbe532f24df534
ericsison <ericsison.dev@gmail.com>
2023-03-08 17:22:57 +0800

    changed ClientsModule.register() to ClientsModule.registerAsync()

--- apps/ims/src/app/services/items/components/details/core/details.module.ts
+++ apps/ims/src/app/services/items/components/details/core/details.module.ts
@@ -1,19 +1,25 @@
 import { MicroserviceClient, MS_CLIENT } from '@gscwd-api/microservices';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { DetailsController } from './details.controller';
 import { DetailsService } from './details.service';
 
 @Module({
   imports: [
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: MS_CLIENT,
-        transport: Transport.REDIS,
-        options: {
-          host: process.env.ITEMS_REDIS_HOST,
-          port: parseInt(process.env.ITEMS_REDIS_PORT),
-          password: process.env.ITEMS_REDIS_PASS,
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => {
+          return {
+            transport: Transport.REDIS,
+            options: {
+              host: configService.getOrThrow<string>('ITEMS_REDIS_HOST'),
+              port: parseInt(configService.getOrThrow<string>('ITEMS_REDIS_PORT')),
+              password: configService.getOrThrow<string>('ITEMS_REDIS_PASS'),
+            },
+          };
         },
       },
     ]),
