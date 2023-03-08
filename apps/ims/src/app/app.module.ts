da6916e3ded556d1e355f3369f134ead8f4fcc2f
ericsison <ericsison.dev@gmail.com>
2023-03-08 20:54:08 +0800

    aggregated all api modules instead of having a dedicated modules array

--- apps/ims/src/app/app.module.ts
+++ apps/ims/src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
-import { DatabaseConfig } from '../config';
-import { SERVICES_MODULES } from '../constants';
+import { ItemsModule } from './services/items';
+import { DatabaseModule } from '../connections';
 
 @Module({
   imports: [
     // config module for reading enironment variables
-    ConfigModule.forRoot({ isGlobal: true, envFilePath: join(__dirname, '../../../apps/ims/.env') }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: join(__dirname, '../../../apps/ims/.env'),
+    }),
 
     // database connection via typeorm
-    TypeOrmModule.forRootAsync({ useClass: DatabaseConfig }),
+    DatabaseModule,
 
     // modules under microservices client
-    ...SERVICES_MODULES,
+    ItemsModule,
   ],
 })
 export class AppModule {}
