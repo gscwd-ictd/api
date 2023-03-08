239a58bf5fb2f36f9a0e4c8195579cd3e40b2fc6
ericsison <ericsison.dev@gmail.com>
2023-03-08 09:37:03 +0800

    modified purchase-request domain

--- apps/procurement/src/app/api/purchase/components/purchase-request/core/purchase-request.controller.ts
+++ apps/procurement/src/app/api/purchase/components/purchase-request/core/purchase-request.controller.ts
@@ -1,50 +1,15 @@
-import { Body, Controller, DefaultValuePipe, Get, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
-import { CreatePurchaseRequestDto } from '../data/pr.dto';
+import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import { CreatePrDto } from '../data/pr.dto';
+import { PurchaseRequestInterceptor } from '../misc/pr.interceptor';
 import { PurchaseRequestService } from './purchase-request.service';
 
 @Controller({ version: '1', path: 'pr' })
 export class PurchaseRequestController {
-  constructor(
-    //
-    private readonly prService: PurchaseRequestService
-  ) {}
+  constructor(private readonly prService: PurchaseRequestService) {}
 
+  @UseInterceptors(PurchaseRequestInterceptor)
   @Post()
-  async create(@Body() prDto: CreatePurchaseRequestDto) {
-    return await this.prService.createPr(prDto);
-  }
-
-  @Get()
-  async findAll(
-    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
-    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number
-  ) {
-    return await this.prService.findAllPrs({ page, limit });
-  }
-
-  @Get(':id')
-  async findById(@Param('id') id: string) {
-    return await this.prService.findPrById(id);
-  }
-
-  // TODO add metadata, so that this route is accessible for dev only
-  // use post
-  @Get('dev/get-val')
-  async getVal() {
-    return await this.prService.getRecentPrCodeSequenceValues();
-  }
-
-  // TODO add metadata, so that this route is accessible for dev only
-  // use post
-  @Get('dev/next-val')
-  async nextVal() {
-    return await this.prService.incrementPrCodeCurrValue();
-  }
-
-  // TODO add metadata, so that this route is accessible for dev only
-  // use post
-  @Get('dev/reset-val')
-  async resetVal() {
-    return await this.prService.resetPrCodeSequenceValues();
+  async create(@Body() prDto: CreatePrDto) {
+    return await this.prService.createRawPr(prDto);
   }
 }
