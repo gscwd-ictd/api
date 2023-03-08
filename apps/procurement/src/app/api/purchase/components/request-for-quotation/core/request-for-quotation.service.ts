5f0d3ea5d26729f966d55209b6282773d9fd380f
ericsison <ericsison.dev@gmail.com>
2023-03-08 09:40:39 +0800

    modified createRfq to call stored function in the database

--- apps/procurement/src/app/api/purchase/components/request-for-quotation/core/request-for-quotation.service.ts
+++ apps/procurement/src/app/api/purchase/components/request-for-quotation/core/request-for-quotation.service.ts
@@ -1,90 +1,30 @@
 import { CrudHelper, CrudService } from '@gscwd-api/crud';
 import { CreateRfqDto, RequestForQuotation } from '@gscwd-api/models';
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
-import { IPaginationOptions } from 'nestjs-typeorm-paginate';
-import { DataSource, EntityManager } from 'typeorm';
-import { RequestedItemService } from '../../requested-item/core/requested-item.service';
-import { CreateQuotationRequestDto } from '../data/rfq.dto';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class RequestForQuotationService extends CrudHelper<RequestForQuotation> {
   constructor(
     // inject crud service
-    private readonly crudService: CrudService<RequestForQuotation>,
-
-    // inject requested items service
-    private readonly requestedItemService: RequestedItemService,
-
-    // inject datasource
-    private readonly datasource: DataSource
+    private readonly crudService: CrudService<RequestForQuotation>
   ) {
     super(crudService);
   }
 
-  async tx_create(manager: EntityManager, rfqDto: CreateRfqDto) {
-    return await this.crudService.transact<RequestForQuotation>(manager).create({
-      dto: rfqDto,
-      onError: () => new BadRequestException(),
-    });
-  }
+  async createRawRfq(rfqDto: CreateRfqDto) {
+    // deconstruct rfqDto object
+    const { prId, items } = rfqDto;
 
-  async tx_findById(manager: EntityManager, id: string) {
-    return await this.crudService.transact<RequestForQuotation>(manager).findOneBy({ findBy: { id }, onError: () => new NotFoundException() });
-  }
+    try {
+      // call the create_pr() stored function
+      const result = await this.crudService.getRepository().query('SELECT * FROM create_rfq($1, $2)', [prId, JSON.stringify(items)]);
 
-  async createRfq(rfqDto: CreateQuotationRequestDto) {
-    // deconstruct rfq object
-    const { items, details } = rfqDto;
+      // this query will return an arrat, thus, return the first element
+      return result[0];
 
-    return await this.datasource.manager.transaction(async (manager) => {
-      // create request for quotation details
-      const requestForQuotation = await this.tx_create(manager, details);
-
-      // await for this process to complete
-      const itemsForQuotation = await Promise.all(
-        // loop through all items
-        items.map(async (item) => {
-          // find requested item by id
-          const requestedItem = await this.requestedItemService.tx_findItemById(manager, item.id);
-
-          // update requested item to add rfq
-          return await this.requestedItemService.tx_setItemForRfq(manager, requestedItem, requestForQuotation);
-        })
-      );
-
-      return { rfq: requestForQuotation, items: itemsForQuotation.length };
-    });
-  }
-
-  async findAllRfqs({ page, limit }: IPaginationOptions) {
-    return await this.crudService.findAll({
-      pagination: { page, limit },
-      find: {
-        relations: { purchaseRequest: { purchaseType: true } },
-        select: {
-          purchaseRequest: {
-            code: true,
-            purchaseType: { type: true },
-            status: true,
-          },
-        },
-      },
-    });
-  }
-
-  async findRfqById(id: string) {
-    return await this.datasource.manager.transaction(async (manager) => {
-      // find rfq details by id
-      const rfq = await this.tx_findById(manager, id);
-
-      // find all items by request_for_quotation id
-      const items = await this.requestedItemService.tx_findItemsByRfqDetailsId(manager, id);
-
-      // get item info via items microservice
-      const itemInfo = await this.requestedItemService.ms_getItemsInfo(items);
-
-      // return resulting values
-      return { rfq, items: itemInfo };
-    });
+      // catch resulting error
+    } catch (error) {
+      throw new BadRequestException(error.message, { cause: new Error() });
+    }
   }
 }
