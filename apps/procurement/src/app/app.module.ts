042c79aa9016ca48141f59437f8bac02e6f201b2
ericsison <ericsison.dev@gmail.com>
2023-03-08 20:49:22 +0800

    aggregated the api modules into app.module instead of having a dedicated modules array

--- apps/procurement/src/app/app.module.ts
+++ apps/procurement/src/app/app.module.ts
@@ -2,16 +2,26 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { join } from 'path';
 import { DatabaseModule } from '../connections/';
-import { API_MODULES } from '../utils/constants';
+import { PurchaseRequestModule, RequestForQuotationModule, PurchaseTypeModule } from './api/purchase/components';
+import { OrgStructureModule } from './services/hrms/components/org-structure';
+import { ItemsModule } from './services/items';
 
 @Module({
   imports: [
     // config module setup for reading env variables
     ConfigModule.forRoot({ isGlobal: true, envFilePath: join(__dirname, '../../../apps/procurement/.env') }),
 
+    // initialize database connection
     DatabaseModule,
 
-    ...API_MODULES,
+    // api modules
+    PurchaseRequestModule,
+    RequestForQuotationModule,
+    PurchaseTypeModule,
+
+    // microservice modules
+    OrgStructureModule,
+    ItemsModule,
   ],
 })
 export class AppModule {}
