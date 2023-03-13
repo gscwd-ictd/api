6a07a8dddde6026b207253dd070085db54427846
dabd101 <dabddelrosario@gmail.com>
2023-03-13 16:14:14 +0800

    removed unit test spec file

--- apps/procurement/src/constants/entities.ts
+++
@@ -1,3 +0,0 @@
-import { PurchaseRequest, RequestForQuotation, RequestedItem, PurchaseType, Supplier } from '@gscwd-api/models';
-
-export const DB_ENTITIES = [PurchaseRequest, RequestedItem, RequestForQuotation, PurchaseType, Supplier];
