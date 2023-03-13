6d71e38edb0cad3c9967399eb119f2bfc98ff4d6
dabd101 <dabddelrosario@gmail.com>
2023-03-13 15:39:16 +0800

    added SuppliersModule

--- apps/procurement/src/app/app.module.ts
+++ apps/procurement/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { join } from 'path';
-import { DatabaseConfig } from '../config';
-import { API_MODULES } from '../constants';
+import { DatabaseModule } from '../connections';
+import { PurchaseRequestModule, RequestForQuotationModule, PurchaseTypeModule } from './api/purchase/components';
+import { SuppliersModule } from './api/suppliers/core/suppliers.module';
+import { OrgStructureModule } from './services/hrms/components/org-structure';
+import { ItemsModule } from './services/items';
 
 @Module({
   imports: [
     // config module setup for reading env variables
     ConfigModule.forRoot({ isGlobal: true, envFilePath: join(__dirname, '../../../apps/procurement/.env') }),
-
-    // typeorm config to connect to postgres db
-    TypeOrmModule.forRootAsync({ useClass: DatabaseConfig }),
 
     DatabaseModule,
 
@@ -18,6 +18,7 @@ import { API_MODULES } from '../constants';
     PurchaseRequestModule,
     RequestForQuotationModule,
     PurchaseTypeModule,
+    SuppliersModule,
 
     // microservice modules
     OrgStructureModule,
