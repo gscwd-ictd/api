bf514d05c71046c18c9b4a335553b2bc74b99975
ericsison <ericsison.dev@gmail.com>
2023-03-13 14:29:06 +0800

    added get route to fetch all requested items by purchase request

--- apps/procurement/src/app/api/purchase/components/requested-item/core/requested-item.controller.ts
+++ apps/procurement/src/app/api/purchase/components/requested-item/core/requested-item.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { RequestedItemService } from './requested-item.service';
 
 @Controller({ version: '1', path: '/requested-items' })
 export class RequestedItemController {
   constructor(private readonly requestedItemService: RequestedItemService) {}
+
+  @Get('pr/:id')
+  async findAllItemsByPrId(@Param('id') id: string) {
+    return await this.requestedItemService.crud().findAll({ find: { where: { prDetails: { id } } } });
+  }
 }
