244befe6c274352cb6b589842b004ff15cf0d8b9
taypskript <alfeche.ictd@gmail.com>
2023-03-16 16:54:50 +0800

    fixed constants module

--- apps/finance/src/constants/modules.ts
+++ apps/finance/src/constants/modules.ts
@@ -2,5 +2,6 @@ import { BudgetTypeModule } from '../api/budget/budget-types';
 import { CostEstimateModule } from '../api/budget/cost-estimates/core/cost-estimates.module';
 import { ChartOfAccountModule } from '../api/account-titles/chart-of-accounts/core/chart-of-accounts.module';
 import { ContraAccountModule } from '../api/account-titles/contra-accounts';
+import { ItemModule } from '../api/item/core/item.module';
 
-export const API_MODULES = [BudgetTypeModule, CostEstimateModule, ContraAccountModule, ChartOfAccountModule];
+export const API_MODULES = [ItemModule, BudgetTypeModule, CostEstimateModule, ContraAccountModule, ChartOfAccountModule];
