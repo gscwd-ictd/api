8aa21176cb27d4f3fb701a91b589a12d8b28bcbf
ericsison <ericsison.dev@gmail.com>
2023-03-21 11:54:45 +0800

    added return types

--- apps/items/src/app/services/items/misc/interceptors/find-all-item-summary.interceptor.ts
+++ apps/items/src/app/services/items/misc/interceptors/find-all-item-summary.interceptor.ts
@@ -1,4 +1,5 @@
 import { ItemsView } from '@gscwd-api/models';
+import { ItemSummary } from '@gscwd-api/utils';
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 import { Pagination } from 'nestjs-typeorm-paginate';
 import { Observable, map } from 'rxjs';
@@ -8,14 +9,15 @@ export class FindAllItemSummariesInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler<unknown>): Observable<unknown> | Promise<Observable<unknown>> {
     return next.handle().pipe(
       map((result: Pagination<ItemsView>) => {
-        const items = result.items.map((item: ItemsView) => ({
+        const items: ItemSummary[] = result.items.map((item: ItemsView) => ({
           id: item.details_id,
           code: `${item.characteristic_code}-${item.classification_code}-${item.category_code}-${item.specification_code}`,
           classification: item.classification_name,
           item: item.category_name,
+          unit: item.unit_symbol,
           details: item.specification_name,
           description: item.description,
-        }));
+        })) as ItemSummary[];
 
         return { ...result, items };
       })
