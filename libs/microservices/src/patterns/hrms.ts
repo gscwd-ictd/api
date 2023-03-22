5f4bf3dc3383ffb2426ce451d29705d29a8e72ab
ericsison <ericsison.dev@gmail.com>
2023-03-22 16:05:10 +0800

    removed unnecesary files

--- libs/microservices/src/patterns/hrms.ts
+++
@@ -1,4 +0,0 @@
-export enum OrganizationalStructurePatterns {
-  GET_ORG = 'get_organization',
-  GET_ORG_BY_ID = 'get_organization_by_id',
-}
--- libs/microservices/src/patterns/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './hrms';
-export * from './items';
