1c41a89ece2ec46016d04c4c3f98256b9b9a47ea
ericsison <ericsison.dev@gmail.com>
2023-03-22 22:11:06 +0800

    modified e2e test

--- apps/procurement/src/app/api/purchase/tests/purchase.e2e.spec.ts
+++ apps/procurement/src/app/api/purchase/tests/purchase.e2e.spec.ts
@@ -11,8 +11,9 @@ import { PurchaseTypeModule } from '../components/purchase-type/core/purchase-ty
 import { PurchaseRequest } from '@gscwd-api/utils';
 import { RequestForQuotationModule } from '../components/request-for-quotation/core/request-for-quotation.module';
 import { RequestedItemModule } from '../../../api/purchase/components/requested-item';
-import { ItemsMicroserviceClientModule } from '@gscwd-api/microservices';
 import { ConfigModule } from '@nestjs/config';
+import { MS_CLIENT } from '@gscwd-api/microservices';
+import { Transport } from '@nestjs/microservices';
 
 let app: INestApplication;
 let datasource: DataSource;
@@ -25,7 +26,6 @@ describe('Purchase Module e2e test', () => {
         PurchaseTypeModule,
         RequestForQuotationModule,
         RequestedItemModule,
-        ItemsMicroserviceClientModule,
         ConfigModule.forRoot({ isGlobal: true }),
         TypeOrmModule.forRoot({
           type: 'postgres',
@@ -38,7 +38,17 @@ describe('Purchase Module e2e test', () => {
           synchronize: true,
         }),
       ],
-    }).compile();
+    })
+      .overrideProvider(MS_CLIENT)
+      .useValue({
+        transport: Transport.REDIS,
+        options: {
+          host: 'localhost',
+          port: 6281,
+          password: 'password',
+        },
+      })
+      .compile();
 
     app = await module.createNestApplication().init();
 
