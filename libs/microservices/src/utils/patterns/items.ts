b257fd2476876034ec231706652f93319f1d8517
ericsison <ericsison.dev@gmail.com>
2023-03-22 15:56:44 +0800

    transferred patterns folder into utils

---
+++ libs/microservices/src/utils/patterns/items.ts
@@ -0,0 +1,59 @@
+export enum ItemsViewPatterns {
+  FIND_ALL = '31B09RDOJI',
+  FIND_BY_ID = 'Y9XUILQCA3',
+  FIND_SUMMARY_BY_ID = 'IFLKQMRFJA',
+  GET_ITEM_BALANCE = 'LPDQJDREVL',
+}
+
+export enum ItemPpeDetailsPatterns {
+  FIND_ALL = 'WPDN914E3V',
+  FIND_BY_ID = 'VGJLPPF5VU',
+}
+
+export enum UnitsOfMeasurePatterns {
+  CREATE = 'KCZS6PBIJQ',
+  FIND_ALL = 'Q6DE3F7BKE',
+  FIND_BY_ID = 'I9FHJ4LUXF',
+  UPDATE = 'MLGLELLCDC',
+  DELETE = 'VCJ1FVV90C',
+}
+
+export enum ItemCharacteristicsPatterns {
+  CREATE = 'I864ZRSN0I',
+  FIND_ALL = '248UCBULM2',
+  FIND_BY_ID = 'WQEWUC5S84',
+  UPDATE = '660JPX90E1',
+  DELETE = '7Z7PJJN8Q8',
+}
+
+export enum ItemClassificationsPatterns {
+  CREATE = '0JANTFR17T',
+  FIND_ALL = '4TYKVUVDDO',
+  FIND_BY_ID = '9XQNUSE1JQ',
+  UPDATE = 'AGYIQ9POFR',
+  DELETE = '65KXCNWC5D',
+}
+
+export enum ItemCategoriesPatterns {
+  CREATE = 'OL05NXWSR7',
+  FIND_ALL = 'CM6NRJM2JL',
+  FIND_BY_ID = '9MNP1GEYVZ',
+  UPDATE = 'XAUBE5PL6Y',
+  DELETE = 'G3VFZCEFUW',
+}
+
+export enum ItemSpecificationsPatterns {
+  CREATE = 'VDGY9GRRF8',
+  FIND_ALL = 'SBZ6UQOP4X',
+  FIND_BY_ID = '87QKFHF562',
+  UPDATE = 'EQIREZO6HK',
+  DELETE = 'PHMKENG86V',
+}
+
+export enum ItemDetailsPatterns {
+  CREATE = 'RT0B0RAKDB',
+  FIND_ALL = 'I3F3AYA1O1',
+  FIND_BY_ID = '0UW2AMNADG',
+  UPDATE = 'M9M5X89TTK',
+  DELETE = 'SYFU2EEJMP',
+}
