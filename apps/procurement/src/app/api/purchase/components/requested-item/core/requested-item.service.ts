ea1da25bee3e772bcfbca936f0fab0f30dabeb62
ericsison <ericsison.dev@gmail.com>
2023-03-22 17:00:40 +0800

    refactored code

--- apps/procurement/src/app/api/purchase/components/requested-item/core/requested-item.service.ts
+++ apps/procurement/src/app/api/purchase/components/requested-item/core/requested-item.service.ts
@@ -1,17 +1,10 @@
 import { CrudHelper, CrudService } from '@gscwd-api/crud';
 import { RequestedItem } from '@gscwd-api/models';
 import { Injectable } from '@nestjs/common';
-import { ItemsService } from '../../../../../services/items';
 
 @Injectable()
 export class RequestedItemService extends CrudHelper<RequestedItem> {
-  constructor(
-    // inject crud service
-    private readonly crudService: CrudService<RequestedItem>,
-
-    // inject items service
-    private readonly itemsService: ItemsService
-  ) {
+  constructor(private readonly crudService: CrudService<RequestedItem>) {
     super(crudService);
   }
 }
