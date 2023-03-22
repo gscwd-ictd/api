0ed41a11278b7d345e2bab4273416613b1bfdc3e
ericsison <ericsison.dev@gmail.com>
2023-03-22 21:55:03 +0800

    added ItemsMicroserviceClientModule

--- apps/procurement/src/app/api/purchase/components/requested-item/core/requested-item.module.ts
+++ apps/procurement/src/app/api/purchase/components/requested-item/core/requested-item.module.ts
@@ -3,9 +3,10 @@ import { CrudModule } from '@gscwd-api/crud';
 import { RequestedItem } from '@gscwd-api/models';
 import { RequestedItemService } from './requested-item.service';
 import { RequestedItemController } from './requested-item.controller';
+import { ItemsMicroserviceClientModule } from '@gscwd-api/microservices';
 
 @Module({
-  imports: [CrudModule.register(RequestedItem)],
+  imports: [CrudModule.register(RequestedItem), ItemsMicroserviceClientModule],
   providers: [RequestedItemService],
   controllers: [RequestedItemController],
   exports: [RequestedItemService],
