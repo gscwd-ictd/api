ac5191e87d5b262bceb22d49d80a9c8b9b6d63fe
ericsison <ericsison.dev@gmail.com>
2023-03-22 16:01:21 +0800

    applied newly created hrms and items client modules, respectively

--- apps/procurement/src/app/services/hrms/components/org-structure/core/org-structure.module.ts
+++ apps/procurement/src/app/services/hrms/components/org-structure/core/org-structure.module.ts
@@ -1,23 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrgStructureService } from './org-structure.service';
 import { OrgStructureController } from './org-structure.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { MicroserviceClient, MS_CLIENT } from '@gscwd-api/microservices';
+import { HrmsMicroserviceClientModule } from '@gscwd-api/microservices';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: MS_CLIENT,
-        transport: Transport.REDIS,
-        options: {
-          host: '192.168.137.249',
-          port: 6100,
-        },
-      },
-    ]),
-  ],
-  providers: [OrgStructureService, MicroserviceClient],
+  imports: [HrmsMicroserviceClientModule],
+  providers: [OrgStructureService],
   controllers: [OrgStructureController],
   exports: [OrgStructureService],
 })
