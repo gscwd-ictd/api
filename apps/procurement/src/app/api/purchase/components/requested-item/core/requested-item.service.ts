b3f037b3f3ca2c5a97fd1b0291b9587a987d9299
ericsison <ericsison.dev@gmail.com>
2023-03-23 13:22:08 +0800

    added more routes

--- apps/procurement/src/app/api/purchase/components/requested-item/core/requested-item.service.ts
+++ apps/procurement/src/app/api/purchase/components/requested-item/core/requested-item.service.ts
@@ -1,10 +1,31 @@
 import { CrudHelper, CrudService } from '@gscwd-api/crud';
 import { RequestedItem } from '@gscwd-api/models';
 import { Injectable } from '@nestjs/common';
+import { ItemsService } from '../../../../../services/items';
 
 @Injectable()
 export class RequestedItemService extends CrudHelper<RequestedItem> {
-  constructor(private readonly crudService: CrudService<RequestedItem>) {
+  constructor(
+    // inject crud service
+    private readonly crudService: CrudService<RequestedItem>,
+
+    // inject microservice client
+    private readonly itemsService: ItemsService
+  ) {
     super(crudService);
+  }
+
+  async findAllItemsByPr(id: string) {
+    const items = (await this.crudService.findAll({
+      find: {
+        where: { prDetails: { id } },
+      },
+    })) as RequestedItem[];
+
+    return await Promise.all(items.map(async (item) => ({ ...item, details: await this.getItemDetails(item.itemId) })));
+  }
+
+  async getItemDetails(id: string) {
+    return await this.itemsService.getItemById(id);
   }
 }
