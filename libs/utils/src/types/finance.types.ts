a3a5d50cb8e41223acda351dcaf926b36f625495
ericsison <ericsison.dev@gmail.com>
2023-04-13 13:36:29 +0800

    added more types in finance and items

--- libs/utils/src/types/finance.types.ts
+++ libs/utils/src/types/finance.types.ts
@@ -15,3 +15,54 @@ export type RawCostEstimate = {
   quantity: number;
   outputPerDay: number;
 };
+
+export type RawProjectDetails = {
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+  id: string;
+  projectName: string;
+  location: string;
+  subject: string;
+  workDescription: string;
+  quantity: number;
+  outputPerDay: number;
+  budgetDetails: {
+    status: string;
+    budgetType: {
+      name: string;
+    };
+    generalLedgerAccount: {
+      name: string;
+    };
+  };
+};
+
+export type ProjectDetailsSummary = {
+  id: string;
+  projectName: string;
+  location: string;
+  subject: string;
+  workDescription: string;
+  status: string;
+};
+
+export type ProjectDetails = {
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+  id: string;
+  projectName: string;
+  location: string;
+  subject: string;
+  workDescription?: string;
+  quantity?: number;
+  outputPerDay?: number;
+  budgetDetails: {
+    status: string;
+    type: string;
+    account: {
+      generalLedgerAccountName: string;
+    };
+  };
+};
