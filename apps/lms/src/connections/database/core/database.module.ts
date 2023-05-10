261f3bb35ad1eb4cc9b4cbaba85dfb0349cc0cc3
taypskript <alfeche.ictd@gmail.com>
2023-05-10 16:41:30 +0800

    set up learning management system

---
+++ apps/lms/src/connections/database/core/database.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DB_ENTITIES } from '../constants/entities';
+import { Environment } from '../constants/environments';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        logging: true,
+        synchronize: true,
+        entities: DB_ENTITIES,
+        replication: {
+          master: {
+            host: configService.getOrThrow<string>(Environment.MASTER_DB_HOST),
+            port: parseInt(configService.getOrThrow<string>(Environment.MASTER_DB_PORT)),
+            username: configService.getOrThrow<string>(Environment.MASTER_DB_USER),
+            password: configService.getOrThrow<string>(Environment.MASTER_DB_PASS),
+            database: configService.getOrThrow<string>(Environment.DB_LMS),
+          },
+          slaves: [
+            {
+              host: configService.getOrThrow<string>(Environment.SLAVE_DB_HOST),
+              port: parseInt(configService.getOrThrow<string>(Environment.SLAVE_DB_PORT)),
+              username: configService.getOrThrow<string>(Environment.SLAVE_DB_USER),
+              password: configService.getOrThrow<string>(Environment.SLAVE_DB_PASS),
+              database: configService.getOrThrow<string>(Environment.DB_LMS),
+            },
+          ],
+        },
+      }),
+    }),
+  ],
+  providers: [],
+})
+export class DatabaseModule {}
