a98c958bb4965bd462ada0831e439a3b3ebca11e
rvnarvaiza <rvnarvaiza@gmail.com>
2023-09-08 08:46:27 +0800

    Added Create Overtime Transaction Function

--- apps/employee-monitoring/src/app/api/overtime/core/overtime.controller.ts
+++ apps/employee-monitoring/src/app/api/overtime/core/overtime.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { CreateOvertimeDto } from '@gscwd-api/models';
+import { Controller, Post } from '@nestjs/common';
 import { OvertimeService } from './overtime.service';
 
 @Controller('overtime')
 export class OvertimeController {
   constructor(private readonly overtimeService: OvertimeService) {}
+
+  @Post()
+  async createOvertime(createOverTimeDto: CreateOvertimeDto) {
+    return await this.overtimeService.createOvertime(createOverTimeDto);
+  }
 }
