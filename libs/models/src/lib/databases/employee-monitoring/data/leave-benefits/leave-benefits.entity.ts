5e4b96fb7fb1379089f34cba5901aaf046493d4f
rvnarvaiza <rvnarvaiza@gmail.com>
2023-12-06 16:24:31 +0800

    Revised Leave Benefits Entity

--- libs/models/src/lib/databases/employee-monitoring/data/leave-benefits/leave-benefits.entity.ts
+++ libs/models/src/lib/databases/employee-monitoring/data/leave-benefits/leave-benefits.entity.ts
@@ -10,7 +10,7 @@ export class LeaveBenefits extends DatabaseEntity {
   @Column({ name: 'leave_name' })
   leaveName: string;
 
-  @Column({ name: 'leave_types', type: 'enum', enum: LeaveTypes })
+  @Column({ name: 'leave_types', type: 'enum', enum: LeaveTypes, nullable: true })
   leaveType: LeaveTypes;
 
   @Column({ name: 'accumulated_credits', type: 'decimal', precision: 3, scale: 2, nullable: true })
@@ -19,10 +19,10 @@ export class LeaveBenefits extends DatabaseEntity {
   @Column({ name: 'credit_distribution', type: 'enum', enum: CreditDistribution, nullable: true })
   creditDistribution: CreditDistribution;
 
-  @Column({ name: 'monetizable' })
+  @Column({ name: 'monetizable', nullable: true })
   isMonetizable: boolean;
 
-  @Column({ name: 'can_be_carried_over' })
+  @Column({ name: 'can_be_carried_over', nullable: true })
   canBeCarriedOver: boolean;
 
   @Column({ name: 'maximum_credits', type: 'decimal', precision: 6, scale: 3, default: 0, nullable: true })
