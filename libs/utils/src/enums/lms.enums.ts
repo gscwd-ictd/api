9b410252431bc856c03cc9ed41c6ef7af0b9ba77
taypskript <alfeche.ictd@gmail.com>
2024-01-12 14:27:19 +0800

    lms - update lms enums of training status

--- libs/utils/src/enums/lms.enums.ts
+++ libs/utils/src/enums/lms.enums.ts
@@ -30,6 +30,7 @@ export enum TrainingPreparationStatus {
 export enum TrainingStatus {
   UPCOMING = 'upcoming',
   ONGOING = 'on going',
+  REQUIREMENTS_SUBMISSION = 'requirements submission',
   COMPLETED = 'completed',
 }
 
