7541a426fa9ead148d1b974ebcafaecbd1118d7b
rvnarvaiza <rvnarvaiza@gmail.com>
2024-03-19 13:26:04 +0800

    Delete route for Events and Announcements

--- apps/employee-monitoring/src/app/api/events-announcements/core/events-announcements.controller.ts
+++ apps/employee-monitoring/src/app/api/events-announcements/core/events-announcements.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param } from '@nestjs/common';
 import { EventsAnnouncementsService } from './events-announcements.service';
 
 @Controller({ version: '1', path: 'events-announcements' })
@@ -8,5 +8,10 @@ export class EventsAnnouncementsController {
   @Get()
   async getEventsAnnouncements() {
     return await this.eventsAnnounceService.getEventsAnnouncements();
+  }
+
+  @Delete(':event_announcement_id')
+  async deleteEventAnnouncement(@Param('event_announcement_id') id: string) {
+    return await this.eventsAnnounceService.deleteEventAnnouncement(id);
   }
 }
--- apps/employee-monitoring/src/app/api/events-announcements/core/events-announcements.service.ts
+++ apps/employee-monitoring/src/app/api/events-announcements/core/events-announcements.service.ts
@@ -1,7 +1,7 @@
 import { CrudHelper, CrudService } from '@gscwd-api/crud';
 import { EventsAnnouncements } from '@gscwd-api/models';
 import { EventsAnnouncementsStatus } from '@gscwd-api/utils';
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 
 @Injectable()
 export class EventsAnnouncementsService extends CrudHelper<EventsAnnouncements> {
@@ -13,5 +13,14 @@ export class EventsAnnouncementsService extends CrudHelper<EventsAnnouncements> 
     return await this.crudService.findAll({
       find: { order: { eventAnnouncementDate: 'DESC' }, where: { status: EventsAnnouncementsStatus.ACTIVE } },
     });
+  }
+
+  async deleteEventAnnouncement(id: string) {
+    const eventAnnouncement = await this.crudService.findOne({ find: { where: { id } } });
+    if (eventAnnouncement) {
+      const result = await this.crudService.delete({ deleteBy: { id }, onError: () => new InternalServerErrorException() });
+      if (result.affected > 0) return eventAnnouncement;
+    }
+    return new InternalServerErrorException();
   }
 }
