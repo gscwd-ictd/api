f2eca59838e89c4cf40a1bf72b15d2c4dc8674b0
john-henry-alfeche <alfeche.ictd@gmail.com>
2024-05-17 11:10:18 +0800

    update learning service provider rating

--- apps/lms/src/app/api/lsp-rating/core/lsp-rating.module.ts
+++ apps/lms/src/app/api/lsp-rating/core/lsp-rating.module.ts
@@ -3,9 +3,10 @@ import { LspRating } from '@gscwd-api/models';
 import { Module } from '@nestjs/common';
 import { LspRatingController } from './lsp-rating.controller';
 import { LspRatingService } from './lsp-rating.service';
+import { TrainingTagsModule } from '../../training/components/tags';
 
 @Module({
-  imports: [CrudModule.register(LspRating)],
+  imports: [CrudModule.register(LspRating), TrainingTagsModule],
   controllers: [LspRatingController],
   providers: [LspRatingService],
   exports: [LspRatingService],
--- apps/lms/src/app/api/lsp-rating/misc/interceptors/find-all-lsp-rating.interceptor.ts
+++ apps/lms/src/app/api/lsp-rating/misc/interceptors/find-all-lsp-rating.interceptor.ts
@@ -2,25 +2,35 @@ import { LspRating } from '@gscwd-api/models';
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { Pagination } from 'nestjs-typeorm-paginate';
 import { Observable, map } from 'rxjs';
+import { TrainingTagsService } from '../../../training/components/tags';
 
 @Injectable()
 export class FindAllLspRatingInterceptor implements NestInterceptor {
+  constructor(private readonly trainingTagsService: TrainingTagsService) {}
   intercept(context: ExecutionContext, next: CallHandler<unknown>): Observable<unknown> | Promise<Observable<unknown>> {
     return next.handle().pipe(
       map(async (result: Pagination<LspRating>) => {
         const items = await Promise.all(
           result.items.map(async (items) => {
+            const trainingTags = await this.trainingTagsService.findAllTagsByTrainingId(items.trainingDetails.id);
+
             return {
               createdAt: items.createdAt,
               updatedAt: items.updatedAt,
               deletedAt: items.deletedAt,
               id: items.id,
+              trainingId: items.trainingDetails.id,
               courseTitle: items.trainingDetails.courseTitle || items.trainingDetails.trainingDesign.courseTitle,
+              numberOfHours: items.trainingDetails.numberOfHours,
+              numberOfParticipants: items.trainingDetails.numberOfParticipants,
+              location: items.trainingDetails.location,
               trainingStart: items.trainingDetails.trainingStart,
               trainingEnd: items.trainingDetails.trainingEnd,
               source: items.trainingDetails.source.name,
               type: items.trainingDetails.type,
               status: items.trainingDetails.status,
+              lspDetails: items.lspDetails.fullName || items.lspDetails.organizationName,
+              trainingTags: trainingTags,
               rating: items.rating,
             };
           })
