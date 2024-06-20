abe3eb6bc2cfbf9cd217dc0aaf435b1a82005a84
gscwd-ictd <gensanwater.ictd@gmail.com>
2024-06-20 16:49:02 +0800

    Revisions on Daily Time Record - Recording of Unfiled undertimes and halfday to ledger

--- apps/employee-monitoring/src/app/api/overtime/components/overtime-application/core/overtime-application.service.ts
+++ apps/employee-monitoring/src/app/api/overtime/components/overtime-application/core/overtime-application.service.ts
@@ -78,20 +78,18 @@ export class OvertimeApplicationService extends CrudHelper<OvertimeApplication> 
       purpose: string;
       status: string;
     }[];
-
     return employees;
   }
 
   async getOvertimeApplicationsByEmployeeIdsByStatus(employeeIds: string[], status: OvertimeStatus) {
     //!TODO INVESTIGATE LATER
-
     const employees = (await this.rawQuery(
       `
-        SELECT DISTINCT overtime_application_id overtimeApplicationId, ois.employee_id_fk employeeId, planned_date plannedDate, estimated_hours estimatedHours, purpose, oa.status status,oapp.remarks remarks 
+        SELECT DISTINCT overtime_application_id overtimeApplicationId, COALESCE(ois.employee_id_fk, oa.manager_id_fk) employeeId, planned_date plannedDate, estimated_hours estimatedHours, purpose, oa.status status,oapp.remarks remarks 
           FROM overtime_application oa 
         INNER JOIN overtime_employee oe ON oa.overtime_application_id = oe.overtime_application_id_fk 
         INNER JOIN overtime_approval oapp ON oapp.overtime_application_id_fk = oa.overtime_application_id
-        INNER JOIN overtime_immediate_supervisor ois ON ois.overtime_immediate_supervisor_id = oa.overtime_immediate_supervisor_id_fk 
+        LEFT JOIN overtime_immediate_supervisor ois ON ois.overtime_immediate_supervisor_id = oa.overtime_immediate_supervisor_id_fk 
         WHERE oe.employee_id_fk IN (?) AND oa.status = ? ORDER BY planned_date ASC;
     `,
       [employeeIds, status]
@@ -103,7 +101,6 @@ export class OvertimeApplicationService extends CrudHelper<OvertimeApplication> 
       purpose: string;
       status: string;
     }[];
-
     return employees;
   }
 }
--- libs/utils/src/enums/employee-monitoring.enum.ts
+++ libs/utils/src/enums/employee-monitoring.enum.ts
@@ -2,3 +2,9 @@ export enum EventsAnnouncementsStatus {
   ACTIVE = 'active',
   INACTIVE = 'inactive',
 }
+
+export enum DtrDeductionType {
+  HALFDAY = 'Half Day',
+  TARDINESS = 'Tardiness',
+  UNDERTIME = 'Undertime',
+}
