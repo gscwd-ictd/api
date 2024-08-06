92c0c13836807e56bba9393964dcf424be3477bd
gscwd-ictd <gensanwater.ictd@gmail.com>
2024-08-06 17:03:59 +0800

    CRON revision for Leave Addback due to  Work Suspension

--- apps/employee-monitoring/src/app/api/leave/components/leave-add-back/core/leave-add-back.service.ts
+++ apps/employee-monitoring/src/app/api/leave/components/leave-add-back/core/leave-add-back.service.ts
@@ -6,13 +6,17 @@ import dayjs = require('dayjs');
 import { EntityManager } from 'typeorm';
 import { WorkSuspensionService } from '../../../../work-suspension/core/work-suspension.service';
 import { LeaveCardLedgerCreditService } from '../../leave-card-ledger-credit/core/leave-card-ledger-credit.service';
+import { EmployeesService } from '../../../../employees/core/employees.service';
+import { EmployeeScheduleService } from '../../../../daily-time-record/components/employee-schedule/core/employee-schedule.service';
 
 @Injectable()
 export class LeaveAddBackService extends CrudHelper<LeaveAddBack> {
   constructor(
     private readonly crudService: CrudService<LeaveAddBack>,
     private readonly workSuspensionService: WorkSuspensionService,
-    private readonly leaveCardLedgerCreditService: LeaveCardLedgerCreditService
+    private readonly leaveCardLedgerCreditService: LeaveCardLedgerCreditService,
+    private readonly employeeScheduleService: EmployeeScheduleService,
+    private readonly employeeService: EmployeesService
   ) {
     super(crudService);
   }
@@ -37,27 +41,28 @@ export class LeaveAddBackService extends CrudHelper<LeaveAddBack> {
     const suspensionHrs = await this.workSuspensionService.getWorkSuspensionBySuspensionDate(dayNow.toDate());
     console.log(suspensionHrs);
 
-    if (suspensionHrs >= 4) {
-      const creditValue = 0.5;
+    if (suspensionHrs > 0) {
       const leaveApplicationDates = (await this.rawQuery(
         `
-          SELECT lad.leave_application_date_id leaveApplicationDatesId 
+          SELECT lad.leave_application_date_id leaveApplicationDatesId    
           FROM leave_application_dates lad
             INNER JOIN leave_application la ON la.leave_application_id = lad.leave_application_id_fk
             INNER JOIN leave_benefits lb ON lb.leave_benefits_id = la.leave_benefits_id_fk 
           WHERE lad.leave_date = ? 
-          AND la.status <> 'cancelled'
-          AND lad.status <> 'cancelled'
+          AND la.status = 'approved'
+          AND lad.status = 'approved'
           AND lad.status <> 'for cancellation'
           AND lb.leave_types <> 'special leave benefit' 
           AND lb.leave_name <> 'Leave Without Pay';
       `,
-        [dayNow.toDate()]
+        [dayNow.format('YYYY-MM-DD')]
       )) as { leaveApplicationDatesId: LeaveApplicationDates }[];
       console.log(leaveApplicationDates);
       if (leaveApplicationDates.length !== 0) {
         const result = await Promise.all(
           leaveApplicationDates.map(async (_leaveApplicationDatesId) => {
+            //const workSuspensionStart = await this.workSuspensionService.GET
+            const creditValue = suspensionHrs / 8;
             const leaveApplicationDatesId = _leaveApplicationDatesId.leaveApplicationDatesId;
             const addBack = await this.crudService.create({
               dto: {
@@ -74,9 +79,7 @@ export class LeaveAddBackService extends CrudHelper<LeaveAddBack> {
             return { ...addBack, leaveCardLedgerCredit };
           })
         );
-
         console.log('--------- Executed Work Suspension CRON JOB -------------');
-        console.log(result);
       } else console.log('--------- Work Suspension CRON JOB not executed because there are no Leave Application on this day-------------');
     } else console.log('--------- Work Suspension CRON JOB not executed because there are no Work Suspension on this day-------------');
   }
