56d06d77436b9435d5fe8e3c59bbaf7f44404b3b
john-henry-alfeche <alfeche.ictd@gmail.com>
2024-08-07 08:24:45 +0800

    added service for updating participant requirements

--- apps/lms/src/app/api/others/components/other-training-participants-requirements/core/other-training-participants-requirements.service.ts
+++ apps/lms/src/app/api/others/components/other-training-participants-requirements/core/other-training-participants-requirements.service.ts
@@ -1,5 +1,9 @@
 import { CrudHelper, CrudService } from '@gscwd-api/crud';
-import { CreateOtherTrainingParticipantsRequirementsDto, OtherTrainingParticipantRequirements } from '@gscwd-api/models';
+import {
+  CreateOtherTrainingParticipantsRequirementsDto,
+  OtherTrainingParticipantRequirements,
+  UpdateOtherTrainingParticipantsRequirementsDto,
+} from '@gscwd-api/models';
 import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { DataSource, EntityManager } from 'typeorm';
 
@@ -52,6 +56,48 @@ export class OtherTrainingParticipantsRequirementsService extends CrudHelper<Oth
     } catch (error) {
       Logger.error(error);
       throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+    }
+  }
+
+  /* update training participants requirements by participants id */
+  async updateParticipantRequirements(data: UpdateOtherTrainingParticipantsRequirementsDto) {
+    try {
+      return await this.dataSource.transaction(async (entityManager) => {
+        const { participants } = data;
+
+        /* insert participants */
+        const updateResults = await Promise.all(
+          participants.map(async (items) => {
+            return await this.crudService.transact<OtherTrainingParticipantRequirements>(entityManager).update({
+              dto: {
+                trainingRequirements: JSON.stringify(items.requirements),
+              },
+              updateBy: {
+                otherTrainingParticipant: {
+                  id: items.participantId,
+                },
+              },
+              onError: (error) => {
+                throw error;
+              },
+            });
+          })
+        );
+
+        // Aggregate results
+        const aggregatedResult = updateResults.reduce(
+          (acc, result) => {
+            acc.affected += result.affected;
+            return acc;
+          },
+          { generatedMaps: [], raw: [], affected: 0 }
+        );
+
+        return aggregatedResult;
+      });
+    } catch (error) {
+      Logger.error(error);
+      throw new HttpException('Bad request.', HttpStatus.BAD_REQUEST);
     }
   }
 }
