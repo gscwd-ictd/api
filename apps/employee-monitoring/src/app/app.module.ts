62649e815aa9cb3464814f44551d3655aa83f336
gscwd-ictd <gensanwater.ictd@gmail.com>
2024-11-05 10:22:14 +0800

    Revised User Logs Module

--- apps/employee-monitoring/src/app/app.module.ts
+++ apps/employee-monitoring/src/app/app.module.ts
@@ -6,18 +6,21 @@ import { join } from 'path';
 import { DatabaseConfig } from '../config/database.config';
 import { appModules } from '../constants/modules';
 import { LoggerMiddleWare } from './logger.service';
+import { ScheduleModule } from '@nestjs/schedule';
+import { UserlogsMiddleware } from './api/user-logs/misc/middlewares/user-logs.middleware';
+import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
 
 const envFilePath = join(__dirname, '../../../apps/employee-monitoring/.env');
-console.log(envFilePath);
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: join(__dirname, '../../../apps/employee-monitoring/.env') }),
     TypeOrmModule.forRootAsync({ useClass: DatabaseConfig }),
     ...appModules,
+    ScheduleModule.forRoot(),
   ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleWare).forRoutes('*');
+    consumer.apply(UserlogsMiddleware).forRoutes('*');
   }
 }
