fbb3c216a3b4cd2d31bae7abeae634a5d0253fcb
john-henry-alfeche <alfeche.ictd@gmail.com>
2024-11-28 08:40:52 +0800

    feat: added sms services

--- apps/sms/src/app/api/sms/sms.service.ts
+++ apps/sms/src/app/api/sms/sms.service.ts
@@ -1,8 +1,5 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
-import { AxiosError } from 'axios';
-import { catchError, firstValueFrom } from 'rxjs';
-import axios from 'axios';
 
 @Injectable()
 export class SmsService {
@@ -15,12 +12,9 @@ export class SmsService {
     const smsIp = process.env.SMS_HOST;
     const smsUser = process.env.SMS_USER;
     const smsPassword = process.env.SMS_PASS;
-    const port = [1, 2, 3, 5];
-    const phoneNumber = '639613522548';
-    const message = 'test asdsadsad';
-
-    const randomPortIndex = Math.floor(Math.random() * port.length);
-    const randomPort = port[randomPortIndex];
+    const port = 1;
+    const phoneNumber = '+639613522548';
+    const message = 'test';
 
     /*  const url = `http://${smsIp}/cgi/WebCGI?1500101=account=${smsUser}&password=${smsPassword}&port=${randomPort}&destination=${phoneNumber}&content=${encodeURIComponent(
       message
@@ -28,53 +22,26 @@ export class SmsService {
 
     const url = `http://${smsIp}/cgi/WebCGI?1500101=account=${encodeURIComponent(smsUser)}&password=${encodeURIComponent(
       smsPassword
-    )}&port=${randomPort}&destination=${phoneNumber}&content=${encodeURIComponent(message)}`;
+    )}&port=${port}&destination=${phoneNumber}&content=${encodeURIComponent(message + port)}`;
 
     try {
-      const response = await axios({
+      const response = await fetch(url, {
         method: 'GET',
-        insecureHTTPParser: false,
-        url: url,
+        headers: {
+          'Content-type': 'text/html',
+        },
       });
 
-      return response;
+      return await response.json();
     } catch (error) {
-      console.error('Error sending SMS:', error);
-      throw error; // Rethrow or handle the error as needed
+      if (error instanceof Error) {
+        // console.error('Error name:', error.name);
+        // console.error('Error message:', error.message);
+        // console.error('Error stack:', error.stack);
+        //console.error('Error code', error.code);
+        //console.error('Error code', error.data);
+        console.error('err', error);
+      }
     }
-
-    // return await axios({
-    //   method: 'get',
-    //   url: url,
-    //   responseType: 'text',
-    // });
-
-    // console.log();
-
-    // await firstValueFrom(
-    //   this.httpService.get(url).pipe(
-    //     catchError((error: AxiosError) => {
-    //       Logger.error(error);
-    //       throw 'An error happened!';
-    //     })
-    //   )
-    // );
-
-    /* try {
-      return this.httpService.get(url);
-      await axios({
-        method: 'get',
-        url: url,
-        responseType: 'json',
-      })
-        .then((response) => {
-          Logger.log(response);
-        })
-        .catch((error) => {
-          Logger.error('AXIOS ERROR! ', error);
-        }); 
-    } catch (error) {
-      console.error('Error sending SMS:', error);
-    }*/
   }
 }
