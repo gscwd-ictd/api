33b561d481d1602171a257c987abe9442cd76580
rvnarvaiza <rvnarvaiza@gmail.com>
2025-02-24 16:17:19 +0800

    Overtime Revisions

--- apps/employee-monitoring/src/app/api/overtime/components/overtime-employee/core/overtime-employee.service.ts
+++ apps/employee-monitoring/src/app/api/overtime/components/overtime-employee/core/overtime-employee.service.ts
@@ -37,11 +37,11 @@ export class OvertimeEmployeeService extends CrudHelper<OvertimeEmployee> {
 
       const overtimeApproval = await this.overtimeApprovalService.crud().findOneOrNull({ find: { where: { managerId, overtimeApplicationId: { id: overtimeApplicationId.toString() } } } });
 
-      if (!overtimeApproval) {
-        throw new RpcException({
-          message: 'Overtime does not exists or user is not the manager of the Overtime Applicant',
-        })
-      }
+      // if (!overtimeApproval) {
+      //   throw new RpcException({
+      //     message: 'Overtime does not exists or user is not the manager of the Overtime Applicant',
+      //   })
+      // }
 
       const overtimeEmployee = await this.crud().findOneOrNull({ find: { where: { overtimeApplicationId: { id: overtimeApplicationId.toString() }, employeeId } } })
       if (!overtimeEmployee) {
