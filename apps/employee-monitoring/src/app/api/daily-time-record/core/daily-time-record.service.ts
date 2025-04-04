a3fd57ebf2ee1a6e7c14065d85ca47e3606bc72b
gscwd-ictd <gensanwater.ictd@gmail.com>
2025-04-04 10:16:20 +0800

    pull from main repo

--- apps/employee-monitoring/src/app/api/daily-time-record/core/daily-time-record.service.ts
+++ apps/employee-monitoring/src/app/api/daily-time-record/core/daily-time-record.service.ts
@@ -78,8 +78,6 @@ export class DailyTimeRecordService extends CrudHelper<DailyTimeRecord> {
     }
   }
 
-  async generateAllEmployeeDtrByMonthAndYear() {}
-
   async getEmployeeDtrByMonthAndYear(companyId: string, year: number, month: number, half: ReportHalf) {
     const daysInMonth = dayjs(year + '-' + month + '-' + '01').daysInMonth();
     const dayRange = this.getDayRange(daysInMonth);
@@ -1252,9 +1250,9 @@ export class DailyTimeRecordService extends CrudHelper<DailyTimeRecord> {
         ) {
           const employeeId = await this.employeeService.getEmployeeIdByCompanyId(companyId);
           const employeeShift = (
-            await this.employeeScheduleService.getEmployeeScheduleByDtrDate(employeeId, dayjs(ivmsEntryItem[0].date).subtract(1, 'day').toDate())
+            await this.employeeScheduleService.getEmployeeScheduleByDtrDate(employeeId, dayjs(ivmsEntryItem.date).subtract(1, 'day').toDate())
           ).schedule.shift;
-
+          console.log(employeeShift);
           if (employeeShift === 'night') {
             if (timeScanTimeOfDay === 'AM') _timeOut = time;
           }
