9e1e0cc783569e892a61cf8e667c126ac80720e5
gscwd-ictd <gensanwater.ictd@gmail.com>
2026-09-10 11:50:33 +0800

    Fix to cron job on leave add back if work suspension is active

--- apps/employee-monitoring/src/app/api/leave/components/leave-add-back/core/leave-add-back.controller.ts
+++ apps/employee-monitoring/src/app/api/leave/components/leave-add-back/core/leave-add-back.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { LeaveAddBackService } from './leave-add-back.service';
 
 @Controller({ version: '1', path: 'leave-add-back' })
@@ -9,5 +9,11 @@ export class LeaveAddBackController {
   async testWorkSuspensionNow(@Param('dtr_date') dtrDate: Date) {
     //return await this.leaveAddBackService.addBackLeaveOnWorkSuspensionV2();
     return await this.leaveAddBackService.addBackLeaveOnWorkSuspension(dtrDate);
+  }
+
+  @Post('run-add-back-work-suspension')
+  async runAddBackManually() {
+    await this.leaveAddBackService.addBackLeaveOnWorkSuspensionV2();
+    return { message: 'Add-back for work suspension executed' };
   }
 }
